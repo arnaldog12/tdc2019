f8d18f4cf33733f399ff0f13dced6665866b6f16
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2019-07-07 10:51:16 -0300

    termina projeto pra CPU

--- TensorFlowMT/TensorFlowCPU/main.cpp
+++ TensorFlowMT/TensorFlowCPU/main.cpp
@@ -1,6 +1,17 @@
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/protobuf/meta_graph.pb.h"
+
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+
+using namespace tensorflow;
+
+GraphDef loadGraph(std::string protobufFile);
+Session* createSession(GraphDef graphDef, SessionOptions options);
+template <class T> Tensor mat2tensor(cv::Mat image, tensorflow::DataType type);
+std::vector<cv::Mat> tensor2mat(Tensor tensor);
+std::vector<cv::Mat> run(Session *session, std::vector<std::pair<std::string, Tensor>> feedDict, std::vector<std::string> outputTensorNames, std::vector<std::string> targetNodeNames);
 
 int main()
 {
@@ -8,14 +19,95 @@ int main()
 	if (!cap.isOpened())
 		if (!cap.open(0)) return 0;
 
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 480);
+
+	GraphDef graphDef = loadGraph("../../models/best_model_ever.pb");
+	Session *session = createSession(graphDef, SessionOptions());
+
 	cv::Mat frame;
 	while (true)
 	{
 		cap >> frame;
 
+		cv::Mat inputImg;
+		cv::cvtColor(frame, inputImg, cv::COLOR_BGR2GRAY);
+		cv::resize(inputImg, inputImg, cv::Size(28, 28), 0, 0, cv::INTER_AREA);
+		inputImg.convertTo(inputImg, CV_32F);
+
+		Tensor imgTensor = mat2tensor<float>(inputImg / 255.0f, tensorflow::DT_FLOAT);
+
+		std::vector<std::pair<std::string, Tensor>> feedDict;
+		feedDict.push_back({ "input_input", imgTensor });
+
+		std::vector<cv::Mat> outputs = run(session, feedDict, {"outputs/Softmax"}, {});
+		cv::Mat output = outputs[0];
+
+		cv::Point maxIdx;
+		cv::minMaxLoc(output, NULL, NULL, NULL, &maxIdx);
+		int y_pred = maxIdx.x;
+		
+		cv::putText(frame, cv::format("%d", y_pred), cv::Point(20, 100), cv::FONT_HERSHEY_TRIPLEX, 4, cv::Scalar(0, 255, 0), 3);
 		cv::imshow("frame", frame);
 		if (cv::waitKey(10) == 27) break;
-	}
+	}	
 
 	return 0;
+}
+
+GraphDef loadGraph(std::string protobufFile)
+{
+	GraphDef graphDef;
+	TF_CHECK_OK(ReadBinaryProto(Env::Default(), protobufFile, &graphDef));
+	return graphDef;
+}
+
+Session* createSession(GraphDef graphDef, SessionOptions options)
+{
+	Session *session;
+	TF_CHECK_OK(NewSession(options, &session));
+	TF_CHECK_OK(session->Create(graphDef));
+	return session;
+}
+
+template <class T>
+Tensor mat2tensor(cv::Mat image, tensorflow::DataType type)
+{
+	T *imageData = (T *)image.data;
+	TensorShape imageShape = TensorShape{ 1, image.rows, image.cols, image.channels() };
+	Tensor imageTensor = Tensor(type, imageShape);
+	std::copy_n((char *)imageData, imageShape.num_elements() * sizeof(T), const_cast<char *>(imageTensor.tensor_data().data()));
+	return imageTensor;
+}
+
+static std::vector<cv::Mat> tensor2mat(Tensor tensor)
+{
+	TensorShape shape = tensor.shape();
+	int nDims = shape.dims();
+
+	int nImages = shape.dim_size(0);
+	int width = nDims > 2 ? shape.dim_size(2) : (nDims > 1 ? shape.dim_size(1) : shape.dim_size(0));
+	int height = nDims > 2 ? shape.dim_size(1) : 1;
+	int channels = (nDims == 4) ? shape.dim_size(3) : 1;
+
+	std::vector<cv::Mat> result;
+	for (int i = 0; i < nImages; i++)
+	{
+		Tensor slice = tensor.Slice(i, i + 1);
+		assert(slice.IsAligned() == true);
+
+		float *outputData = slice.flat<float>().data();
+		cv::Mat imgOut(cv::Size(width, height), CV_32FC(channels));
+		std::copy_n((char*)outputData, slice.shape().num_elements() * sizeof(float), (char*)imgOut.data);
+		result.push_back(imgOut);
+	}
+
+	return result;
+}
+
+std::vector<cv::Mat> run(Session *session, std::vector<std::pair<std::string, Tensor>> feedDict, std::vector<std::string> outputTensorNames, std::vector<std::string> targetNodeNames)
+{
+	std::vector<Tensor> outputsTensor;
+	TF_CHECK_OK(session->Run(feedDict, outputTensorNames, targetNodeNames, &outputsTensor));
+	return tensor2mat(outputsTensor[0]);
 }
