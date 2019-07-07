84d817507576d2db7a212c48767907d99c62d85c
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2019-07-07 14:54:51 -0300

    builda projeto arm

--- TensorFlowMT/TensorFlowARM/TensorFlowARM.cpp
+++ TensorFlowMT/TensorFlowARM/TensorFlowARM.cpp
@@ -1,40 +1,21 @@
 #include "TensorFlowARM.h"
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/protobuf/meta_graph.pb.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "TensorFlowARM", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "TensorFlowARM", __VA_ARGS__))
 
-extern "C" {
-	/* This trivial function returns the platform ABI for which this dynamic native library is compiled.*/
-	const char * TensorFlowARM::getPlatformABI()
+extern "C" 
+{
+	int predictDigit(cv::Mat frame)
 	{
-	#if defined(__arm__)
-	#if defined(__ARM_ARCH_7A__)
-	#if defined(__ARM_NEON__)
-		#define ABI "armeabi-v7a/NEON"
-	#else
-		#define ABI "armeabi-v7a"
-	#endif
-	#else
-		#define ABI "armeabi"
-	#endif
-	#elif defined(__i386__)
-		#define ABI "x86"
-	#else
-		#define ABI "unknown"
-	#endif
-		LOGI("This dynamic shared library is compiled with ABI: %s", ABI);
-		return "This native library is compiled with ABI: %s" ABI ".";
-	}
+		cv::Mat inputImg;
 
-	void TensorFlowARM()
-	{
-	}
-
-	TensorFlowARM::TensorFlowARM()
-	{
-	}
-
-	TensorFlowARM::~TensorFlowARM()
-	{
+		cv::cvtColor(frame, inputImg, cv::COLOR_BGR2GRAY);
+		cv::resize(inputImg, inputImg, cv::Size(28, 28), 0, 0, cv::INTER_AREA);
+		inputImg.convertTo(inputImg, CV_32F);
+		return 0;
 	}
 }
--- TensorFlowMT/TensorFlowARM/TensorFlowARM.h
+++ TensorFlowMT/TensorFlowARM/TensorFlowARM.h
@@ -1,10 +1,15 @@
 #pragma once
 
-class TensorFlowARM
-{
-public:
-	const char * getPlatformABI();
-	TensorFlowARM();
-	~TensorFlowARM();
-};
+#include "opencv2/core/core.hpp"
 
+#ifdef __cplusplus
+// only need to export C interface if // used by C++ source code
+extern "C"
+{
+#endif
+
+	int predictDigit(cv::Mat frame);
+
+#ifdef __cplusplus
+}
+#endif
